d3500db1939c5a7eb11a6d4c6d726fb5f11c3cc7
Sebastian Peña Doria - Ceiba Software <sebastian.pena@ceiba.com.co>
2021-06-17 15:46:27 -0500

    Configured beans at infrastructure module

--- PcStore/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ PcStore/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -1,5 +1,9 @@
 package com.ceiba.configuracion;
 
+import com.ceiba.pcstore.port.repository.IBuyerDataRepository;
+import com.ceiba.pcstore.port.repository.IComponentRepository;
+import com.ceiba.pcstore.port.repository.IOrderRepository;
+import com.ceiba.pcstore.service.*;
 import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
 import com.ceiba.usuario.servicio.ServicioCrearUsuario;
@@ -9,6 +13,32 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
+
+    @Bean
+    public CreateOrderService createOrderService(IOrderRepository orderRepository) {
+        return new CreateOrderService(orderRepository);
+    }
+
+    @Bean
+    public CreateBuyerDataService createBuyerDataService(IBuyerDataRepository buyerDataRepository) {
+        return new CreateBuyerDataService(buyerDataRepository);
+    }
+
+    @Bean
+    public GetOrderService getOrderService(IOrderRepository orderRepository) {
+        return new GetOrderService(orderRepository);
+    }
+
+    @Bean
+    public GetComponentListService getComponentListService(IComponentRepository componentRepository) {
+        return new GetComponentListService(componentRepository);
+    }
+
+    @Bean
+    public GetComponentService getComponentService(IComponentRepository componentRepository) {
+        return new GetComponentService(componentRepository);
+    }
+    
 
     @Bean
     public ServicioCrearUsuario servicioCrearUsuario(RepositorioUsuario repositorioUsuario) {
