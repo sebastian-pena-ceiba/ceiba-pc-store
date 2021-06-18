b6c67c0fcd06b20fc83a827d546cac8e44e03cd2
Sebastian Peña Doria - Ceiba Software <sebastian.pena@ceiba.com.co>
2021-06-18 09:39:16 -0500

    Updated ComponentController according new GetComponentManager and and ListCompoentManager

--- PcStore/infraestructura/src/main/java/com/ceiba/pcstore/controller/ComponentController.java
+++ PcStore/infraestructura/src/main/java/com/ceiba/pcstore/controller/ComponentController.java
@@ -1,8 +1,8 @@
 package com.ceiba.pcstore.controller;
 
+import com.ceiba.pcstore.command.manager.GetComponentManager;
+import com.ceiba.pcstore.command.manager.ListComponentManager;
 import com.ceiba.pcstore.model.entity.Component;
-import com.ceiba.pcstore.service.GetComponentListService;
-import com.ceiba.pcstore.service.GetComponentService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,22 +11,22 @@ import java.util.List;
 @RequestMapping("/components")
 public class ComponentController {
 
-    private GetComponentListService getComponentListService;
-    private GetComponentService getComponentService;
+    private ListComponentManager listComponentManager;
+    private GetComponentManager getComponentManager;
 
-    public ComponentController(GetComponentListService getComponentListService, GetComponentService getComponentService) {
-        this.getComponentListService = getComponentListService;
-        this.getComponentService = getComponentService;
+    public ComponentController(ListComponentManager listComponentManager, GetComponentManager getComponentManager) {
+        this.listComponentManager = listComponentManager;
+        this.getComponentManager = getComponentManager;
     }
 
     @GetMapping
     public List<Component> listComponents(@RequestParam(name = "type", required = false) String type) {
-        return getComponentListService.execute(type);
+        return listComponentManager.ejecutar(type);
     }
 
     @GetMapping("/{idComponent}")
     public Component getComponent(@PathVariable("idComponent") Long id) {
-        return getComponentService.execute(id);
+        return getComponentManager.ejecutar(id);
     }
 
 }
