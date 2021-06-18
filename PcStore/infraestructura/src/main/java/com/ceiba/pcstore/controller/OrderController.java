7a61882a1d85880fab4c917b9f2744ef06e14aa6
Sebastian Peña Doria - Ceiba Software <sebastian.pena@ceiba.com.co>
2021-06-18 14:54:10 -0500

    Added Order Controller

---
+++ PcStore/infraestructura/src/main/java/com/ceiba/pcstore/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.ceiba.pcstore.controller;
+
+import com.ceiba.ComandoRespuesta;
+import com.ceiba.pcstore.command.OrderCommand;
+import com.ceiba.pcstore.command.manager.CreateOrderManager;
+import com.ceiba.pcstore.command.manager.GetOrderManager;
+import com.ceiba.pcstore.model.dto.OrderDto;
+import com.ceiba.pcstore.model.entity.Order;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private CreateOrderManager createOrderManager;
+    private GetOrderManager getOrderManager;
+
+    public OrderController(CreateOrderManager createOrderManager, GetOrderManager getOrderManager) {
+        this.createOrderManager = createOrderManager;
+        this.getOrderManager = getOrderManager;
+    }
+
+    @PostMapping
+    public ComandoRespuesta<OrderCommand> createOrder(@RequestBody OrderCommand orderCommand) {
+        return createOrderManager.ejecutar(orderCommand);
+    }
+
+    @GetMapping("/{code}")
+    public OrderDto getOrder(@PathVariable("code") String trackingCode) {
+        return getOrderManager.ejecutar(trackingCode);
+    }
+
+}
